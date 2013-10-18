7e1ab8fa66121e39338a449786048d6f08d895b6
Gustavo Campeol <gustavocampeol@MacBook-Pro-de-Gustavo-Campeol.local>
2013-10-17 23:39:45 -0300

    added a test class

---
+++ CCteste.h
@@ -0,0 +1,13 @@
+//
+//  CCteste.h
+//  Git Test
+//
+//  Created by Gustavo Campeol on 17/10/13.
+//  Copyright (c) 2013 Gustavo Campeol. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCteste : NSObject
+
+@end
---
+++ CCteste.m
@@ -0,0 +1,13 @@
+//
+//  CCteste.m
+//  Git Test
+//
+//  Created by Gustavo Campeol on 17/10/13.
+//  Copyright (c) 2013 Gustavo Campeol. All rights reserved.
+//
+
+#import "CCteste.h"
+
+@implementation CCteste
+
+@end
